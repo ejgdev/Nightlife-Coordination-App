6d619ae92d146661c3837eafe2ea010c84b29b6b
edlobox <lobox.ed@gmail.com>
2018-02-22 18:47:44 -0500

    changing to a clean app

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,44 +1,29 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
+		.get(function (req, res) {
+			res.render('index.ejs',{
+	      userLogged: req.isAuthenticated(),
+	      user: req.user
+	    });
 		});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+		app.route('/login')
+			.get((req, res) => res.redirect('/'));
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+		app.route('/logout')
+			.get((req, res) => {
+				req.logout();
+				res.redirect('/');
+			});
 
 	app.route('/profile')
 		.get(isLoggedIn, function (req, res) {
 			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
 		});
 
 	app.route('/auth/github')
@@ -49,9 +34,12 @@ module.exports = function (app, passport) {
 			successRedirect: '/',
 			failureRedirect: '/login'
 		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
+
+function isLoggedIn (req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	} else {
+		res.redirect('/login');
+	}
+}
--- server.js
+++ server.js
@@ -5,16 +5,20 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true})); // true for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, {useMongoClient: true});
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/views', express.static(process.cwd() + '/views'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.use(session({
@@ -30,5 +34,5 @@ routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+	console.log('Server listening on port ' + port + '...');
 });
